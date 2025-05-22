543e876d97f37de02236658d02d52bc821edba20
AdilsonKassopa <AdilsonKassopa@gmail.com>
2025-05-22 19:56:42 +0100

    trabalhei na função para parar o cronomoetro do pomodoro

--- SRC/js/pomodoro.js
+++ SRC/js/pomodoro.js
@@ -1,31 +1,46 @@
 let buttonStartPomodoro = document.getElementById("startPomodoro");
+let buttonStopPomodoro = document.getElementById("stopPomodoro")
 let intervalo = null;
 let minutosElement = document.getElementById("minutos");
 let segundosElement = document.getElementById("segundos");
 let minutos = parseInt(minutosElement.textContent)
 let segundos = 60
-console.log(minutos);
-console.log(minutosElement);
+let interval = null
 
 
 
 function iniciarPomodoro() {
-    setInterval(()=>{
+    interval = setInterval(()=>{
+    console.log(interval);
+    
+    if(interval != null ){
         segundos--
         if(segundos == 59 ){
             minutos--
         }
         minutosElement.textContent = minutos
         segundosElement.textContent = segundos
+        if(minutos == 0 && segundos == 0){
+            console.log(interval);
+            clearInterval(interval)
+            interval = null
+        }
         if(segundos == 0){
             segundos = 60
-        }else if(minutos == 0 && segundos == 0){
-            setTimeout()
         }
+    }
             
     },1000)
 }
 
+function pararPomodoro(){
+    if(interval != null){
+        clearInterval(interval)
+        interval = null
+    }
+}
+
 buttonStartPomodoro.onclick = iniciarPomodoro
+buttonStopPomodoro.onclick = pararPomodoro
 
 
